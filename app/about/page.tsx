31e92de762f6dcab05cc937ec7e388726c22dd3d
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-02-25 23:45:40 +0600

    i

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  
+}
+
+const About = ({}: Props) => {
+  return (
+    <div>
+      this is about page 
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa tempore, fugit ullam sequi est nulla quod hic suscipit odit nobis, error at voluptas et. Reiciendis alias accusantium deserunt voluptates corrupti?
+      </p>
+    </div>
+  );
+};
+
+export default About;
