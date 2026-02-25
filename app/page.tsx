0c2f557f1f13ba034989e130c4f8a732ad0e1773
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-02-26 00:24:10 +0600

    ray light completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 interface Props {}
 
 const Home = ({}: Props) => {
-  return <h1>Welcome to next js new project</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br />
+        Event You Can't Miss
+      </h1>
+      <p className="text-center ml-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+    </section>
+  );
 };
 
 export default Home;
