38ae4374d1b2b81b213f7a56d600e59362447a88
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-02-28 14:03:44 +0600

    finalize the app

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async ({}: Props) => {
   "use cache";
   cacheLife("hours");
-  // const response = await fetch(`${BASE_URL}/api/events`);
-  // const { events } = await response.json();
-  let events: any = []
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
@@ -27,13 +26,13 @@ const Page = async ({}: Props) => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events list-none   ">
-          {/* {events &&
+          {events &&
             events.length > 0 &&
             events.map((event: IEvent) => (
               <li key={event.title} className="list-none">
                 <EventCard {...event} />
               </li>
-            ))} */}
+            ))}
         </ul>
       </div>
     </section>
